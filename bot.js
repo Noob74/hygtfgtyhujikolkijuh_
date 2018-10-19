ea955709fe08aac0aa1e3a3ce9408ef2bc1297ae
Noob74 <43444995+Noob74@users.noreply.github.com>
2018-10-19 16:43:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
  console.log(`----------------`);
       console.log(`Desert Bot- Script By : i1Suhaib`);
         console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+      console.log(`ON ${client.guilds.size}
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setGame(`Over Power Shop`,"http://twitch.tv/S-F")
